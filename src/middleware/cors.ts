f882111466f3b74cbb03b5dce8dc1c53e4e7437e
codecShivam <codecshivam@gmail.com>
2025-09-19 00:07:50 +0530

    fix: cors

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,7 +3,9 @@ import { cors } from 'hono/cors';
 export const corsMiddleware = cors({
   origin: [
     "https://siwe-ui.pages.dev",
-    "https://siwe-ui.pages.dev/*"
+    "https://siwe-ui.pages.dev/*",
+    "https://codecshivam.xyz/*",
+    "https://codecshivam.xyz"
   ],
   credentials: true,
 }); 
